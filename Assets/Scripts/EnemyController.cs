eac084902f56e1107db19a6d8496d33fa45618ea
sssmart24 <97680355+sssmart24@users.noreply.github.com>
2022-01-27 14:07:55 -0500

    changed particles

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -8,6 +8,8 @@ public class EnemyController : MonoBehaviour
     public bool vertical;
     public float changeTime = 3.0f;
 
+    public ParticleSystem smokeEffect;
+
     Rigidbody2D rigidbody2D;
     float timer;
     int direction = 1;
@@ -83,5 +85,6 @@ public class EnemyController : MonoBehaviour
         rigidbody2D.simulated = false;
         //optional if you added the fixed animation
         animator.SetTrigger("Fixed");
+        smokeEffect.Stop();
     }
 }
